fbc96300191f9b4a8baf46090f32303d04d12230
steve <stevenzearo@gmail.com>
2019-10-25 18:05:13 +0800

    gc

---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author steve
+ */
+public class Test {
+    public static void main(String[] args) {
+        var s = """
+                这是行字符串
+                """;
+        System.out.println(s);
+    }
+}
